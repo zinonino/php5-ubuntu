a6c53639f06452ea5b69d43e968d20b50fb7cb89
zinonino <zinonino@abv.bg>
2022-09-22 18:26:40 +0300

    Update install-php.sh

--- scripts/install-php.sh
+++ scripts/install-php.sh
@@ -2,9 +2,8 @@
 debconf_fix="DEBIAN_FRONTEND=noninteractive"
 dpkg_install="sudo $debconf_fix dpkg -i --force-conflicts --force-overwrite"
 sudo mkdir -p /var/run /run/php /usr/local/php /usr/lib/systemd/system /usr/lib/cgi-bin /var/www/html
-[[ "$VERSION_ID" = "20.04" || "$VERSION_ID" = "22.04" ]] && $dpkg_install ./deps/"$VERSION_ID"/multiarch-support_2.28-10_amd64
-##$dpkg_install ./deps/"$VERSION_ID"/*.deb
+[[ "$VERSION_ID" = "20.04" || "$VERSION_ID" = "22.04" || "$VERSION_ID" = "11" ]] && $dpkg_install ./deps/"$VERSION_ID"/multiarch-support_2.28-10_amd64
+$dpkg_install ./deps/"$VERSION_ID"/*.deb
 ##$dpkg_install ./deps/all/*.deb
-dpkg_install ./deps/debian/*.deb
 sudo tar -I zstd -xf ./php-@PHP_VERSION@-build.tar.zst -C /usr/local/php
 sudo ln -sf /usr/local/php/@PHP_VERSION@/etc/php.ini /etc/php.ini
